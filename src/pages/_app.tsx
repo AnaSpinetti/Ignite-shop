307b18b5e117755b0346dd60457c61232ff0e7a3
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-10-20 01:54:33 -0300

    Configuração stitches e criação do header

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from "next/app";
+import {globalStyles} from "../styles/global";
+import { Container, Header } from "../styles/pages/app";
+import logoImg from '../assets/logo.svg';
+import Image from "next/image";
+
+globalStyles()
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
+}
+
+export default MyApp
